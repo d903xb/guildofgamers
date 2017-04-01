00cfa20b0fdc0a9ea1df12783342e065c691940d
Steve Day <smday@Steves-MacBook-Pro.local>
2017-04-01 18:38:02 +0100

    Project creation

---
+++ src/main/java/org/guildofgamers/AssassinsApplication.java
@@ -0,0 +1,12 @@
+package org.guildofgamers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssassinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssassinsApplication.class, args);
+	}
+}
